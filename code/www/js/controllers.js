c37dad2c7de64ab05c6957f51915e03d6c61221e
cj xonix <badbiz1988@gmail.com>
2015-09-11 00:04:59 +0100

    safe save testing after

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -1,25 +1,118 @@
 angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 
 
-/*
-Controller for the discover page
-*/
-.controller('DiscoverCtrl', function($scope) {
+    /*
+     Controller for the discover page
+     */
+    .controller('DiscoverCtrl', function ($scope,$ionicLoading, $timeout,User,Recommendations) {
+        // helper functions for loading
+        var showLoading = function() {
+            $ionicLoading.show({
+                template: '<i class="ion-loading-c"></i>',
+                noBackdrop: true
+            });
+        }
 
-})
+        var hideLoading = function() {
+            $ionicLoading.hide();
+        }
+
+        // set loading to true first time while we retrieve songs from server.
+        showLoading();
+        //..........loading
+
+        // get our first songs
+        Recommendations.init()
+            .then(function(){
+                $scope.currentSong = Recommendations.queue[0];
+                Recommendations.playCurrentSong();
+            }).then(function(){
+                // turn loading off
+                hideLoading();
+                $scope.currentSong.loaded = true;
+            });
+
+        // initialize the current song
+        //$scope.currentSong = angular.copy($scope.songs[0]);
+
+        $scope.sendFeedback = function (bool) {
+            // first, add to favorites if they favorited
+            if (bool) User.addSongToFavorites($scope.currentSong);
+            // set variable for the correct animation sequence
+            $scope.currentSong.rated = bool;
+            $scope.currentSong.hide = true;
+
+            $timeout(function () {
+                // $timeout to allow animation to complete before changing to next song
+                // set the current song to one of our three songs
+                //var randomSong = Math.round(Math.random() * ($scope.songs.length - 1));
+                // prepare the next song
+                Recommendations.nextSong();
+                // update current song in scope
+               // $scope.currentSong = angular.copy($scope.songs[randomSong]);
+                $scope.currentSong = Recommendations.queue[0];
+
+                $scope.currentSong.loaded = false;
 
 
-/*
-Controller for the favorites page
-*/
-.controller('FavoritesCtrl', function($scope) {
+            }, 250);
 
-})
+            Recommendations.playCurrentSong().then(function() {
+                $scope.currentSong.loaded = true;
+            });
+
+        };
+
+        // used for retrieving the next album image.
+        // if there isn't an album image available next, return empty string.
+        $scope.nextAlbumImg = function() {
+            if (Recommendations.queue.length > 1) {
+                return Recommendations.queue[1].image_large;
+            }
+
+            return '';
+        };
 
 
-/*
-Controller for our tab bar
-*/
-.controller('TabsCtrl', function($scope) {
 
-});
+    })
+
+
+    /*
+     Controller for the favorites page
+     */
+    .controller('FavoritesCtrl', function ($scope, $timeout, User) {
+        // get the list of our favorites from the user service
+        $scope.favorites = User.favorites;
+
+        $scope.removeSong = function(song, index) {
+            User.removeSongFromFavorites(song, index);
+        }
+
+        $scope.openSong = function(song) {
+            $window.open(song.open_url, "_system");
+        }
+
+
+    })
+
+
+    /*
+     Controller for our tab bar
+     */
+    .controller('TabsCtrl', function ($scope,Recommendations,User) {
+
+        // expose the number of new favorites to the scope
+        $scope.favCount = User.favoriteCount;
+
+        // stop audio when going to favorites page
+        $scope.enteringFavorites = function() {
+            User.newFavorites = 0;
+            Recommendations.haltAudio();
+        }
+
+        $scope.leavingFavorites = function() {
+            Recommendations.init();
+        }
+
+    });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,1 +1,101 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', [])
+    .factory('User', function () {
+
+        var o = {
+            username: false,
+            session_id: false,
+            favorites: [],
+            newFavorites: 0
+        };
+
+        o.addSongToFavorites = function(song) {
+            // make sure there's a song to add
+            if (!song) return false;
+
+            // add to favorites array
+            o.favorites.unshift(song);
+            o.newFavorites++;
+        };
+
+        o.removeSongFromFavorites = function(song, index) {
+            // make sure there's a song to add
+            if (!song) return false;
+
+            // add to favorites array
+            o.favorites.splice(index, 1);
+        };
+
+        o.favoriteCount = function() {
+            return o.newFavorites;
+        };
+
+        return o;
+    })
+    .factory('Recommendations', function($http,$q, SERVER) {
+        var media;
+
+        var o = {
+            queue: []
+        };
+
+        o.getNextSongs = function() {
+            return $http({
+                method: 'GET',
+                url: SERVER.url + '/recommendations'
+            }).success(function(data){
+                // merge data into the queue
+                o.queue = o.queue.concat(data);
+            });
+        };
+
+        o.nextSong = function() {
+            // pop the index 0 off
+            o.queue.shift();
+
+            // end the song
+            o.haltAudio();
+
+            // low on the queue? lets fill it up
+            if (o.queue.length <= 3) {
+                o.getNextSongs();
+            }
+
+        };
+
+
+        o.playCurrentSong = function() {
+            var defer = $q.defer();
+
+            // play the current song's preview
+            media = new Audio(o.queue[0].preview_url);
+
+            // when song loaded, resolve the promise to let controller know.
+            media.addEventListener("loadeddata", function() {
+                defer.resolve();
+            });
+
+            media.play();
+
+            return defer.promise;
+        };
+
+        // used when switching to favorites tab
+        o.haltAudio = function() {
+            if (media) media.pause();
+        };
+
+        o.init = function() {
+            if (o.queue.length === 0) {
+                // if there's nothing in the queue, fill it.
+                // this also means that this is the first call of init.
+                return o.getNextSongs();
+
+            } else {
+                // otherwise, play the current song
+                return o.playCurrentSong();
+            }
+        };
+
+        return o;
+    })
+;
